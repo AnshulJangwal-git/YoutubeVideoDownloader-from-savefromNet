5dc01cca5016731c8354810393ae1805398af056
anshul <anshuljangwal006@gmail.com>
2021-06-05 20:31:15 +0530

    video downloaded successfully !!

---
+++ Hack1/Youtubedownloader.js
@@ -0,0 +1,62 @@
+const puppeteer = require("puppeteer") ;
+
+
+let ytLinks = require("./ytLinks") ;
+
+(async function downloader(){
+try{
+
+    let browser = await puppeteer.launch({
+        headless : false ,
+        defaultViewport : null,
+        args : ["--start maximised"] ,
+        slowMo : 12
+    });
+
+    let pages = await browser.pages() ;
+    let tab = pages[0] ;
+
+    await tab.goto("https://en.savefrom.net/1-youtube-video-downloader-5/") ;
+
+    await tab.waitForTimeout(2000) ;
+    // await tab.type(".tarea-wrap input", "https://youtu.be/n1PCW0C1aiM") ;
+
+    const videoDownloaderLink = "https://en.savefrom.net/1-youtube-video-downloader-5/";
+
+    for(let i = 0; i < ytLinks.length; i++){
+        await addSongsLinks(browser, videoDownloaderLink, ytLinks[i]) ;
+    }
+
+    
+
+
+}catch(error){
+    console.log(error) ;
+}
+})();
+
+async function addSongsLinks(browser,videoDownloaderLink ,ytLink){
+    try{
+        let newTab = await browser.newPage() ;
+
+        await newTab.goto(videoDownloaderLink) ;
+
+        let songLink = ytLink["slink"] ;
+
+        await newTab.waitForTimeout(2000) ;
+
+        await newTab.type(".tarea-wrap input", songLink) ;
+
+        await newTab.waitForSelector(".r-box button", {visible : true}) ;
+        await newTab.click(".r-box button") ;
+
+        await newTab.waitForTimeout(2000) ;
+        await newTab.click(".def-btn-box a") ;
+
+        await newTab.close() ;
+
+
+    }catch(error){
+        console.log(error) ;
+    }
+}
---
+++ Hack1/ytLinks.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        "slink" : "https://youtu.be/n1PCW0C1aiM" 
+    },
+    {
+        "slink" : "https://youtu.be/J6enOG547lk" 
+    },
+    {
+        "slink" : "https://youtu.be/4fndeDfaWCg" 
+    },
+    {
+        "slink" : "https://youtu.be/aBt8fN7mJNg" 
+    },
+    {
+        "slink" : "https://youtu.be/o_v9MY_FMcw" 
+    }
+]
